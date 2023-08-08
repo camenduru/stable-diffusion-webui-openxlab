b00173ac5bef538945368961739f6bbdcaf5f855
camenduru <54370274+camenduru@users.noreply.github.com>
2023-08-09 01:24:44 +0300

    test

--- header.py
+++ header.py
@@ -1,10 +1,6 @@
         with gr.Box():
             gr.HTML(f'''
             <div class="gr-prose" style="max-width: 80%">
-                <p>
-                    🐣 Please follow me for new updates <a href="https://twitter.com/camenduru" style="target=" _blank"="">https://twitter.com/camenduru</a>&nbsp;&nbsp;
-                    🔥 Please join our discord server <a href="https://discord.gg/k5BwmmvJJU" style="target=" _blank"="">https://discord.gg/k5BwmmvJJU</a>&nbsp;&nbsp;
-                    🥳 Please join my patreon community <a href="https://patreon.com/camenduru" style="target=" _blank"="">https://patreon.com/camenduru</a>&nbsp;&nbsp;
-                </p>
+                <p>🐣 Please follow me for new updates <a href="https://twitter.com/camenduru" style="target=" _blank"="">https://twitter.com/camenduru</a>&nbsp;&nbsp;🔥 Please join our discord server <a href="https://discord.gg/k5BwmmvJJU" style="target=" _blank"="">https://discord.gg/k5BwmmvJJU</a>&nbsp;&nbsp;🥳 Please join my patreon community <a href="https://patreon.com/camenduru" style="target=" _blank"="">https://patreon.com/camenduru</a>&nbsp;&nbsp;</p>
             </div>
             ''')
