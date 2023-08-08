4af569faa1db4927f0668081b92fa5c35d700fec
camenduru <54370274+camenduru@users.noreply.github.com>
2023-08-09 01:42:35 +0300

    test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 🚦WIP🚦
+🐣 Please follow me for new updates https://twitter.com/camenduru <br />
+🔥 Please join our discord server https://discord.gg/k5BwmmvJJU <br />
+🥳 Please join my patreon community https://patreon.com/camenduru <br />
+
+## Main Repo
+https://github.com/AUTOMATIC1111/stable-diffusion-webui (Thanks to @AUTOMATIC1111 ❤)
--- header.py
+++ header.py
@@ -1,6 +1,7 @@
-        with gr.Box():
-            gr.HTML(f'''
-            <div class="gr-prose" style="max-width: 80%">
-                <p>🐣 Please follow me for new updates <a href="https://twitter.com/camenduru" style="target=" _blank"="">https://twitter.com/camenduru</a>&nbsp;&nbsp;🔥 Please join our discord server <a href="https://discord.gg/k5BwmmvJJU" style="target=" _blank"="">https://discord.gg/k5BwmmvJJU</a>&nbsp;&nbsp;🥳 Please join my patreon community <a href="https://patreon.com/camenduru" style="target=" _blank"="">https://patreon.com/camenduru</a>&nbsp;&nbsp;</p>
-            </div>
+        with gr.Group():
+            gr.Markdown(f'''
+              🐣 Please follow me for new updates https://twitter.com/camenduru
+              🔥 Please join our discord server https://discord.gg/k5BwmmvJJU
+              🥳 Please join my patreon community https://patreon.com/camenduru
             ''')
+
